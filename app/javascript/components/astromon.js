165d11d02c28c4dec9377d0a9a303b0d1725e28e
Leon Lee <jtllee1@gmail.com>
2020-02-01 23:48:21 +0000

    changed javascript to allow display of gems on click for first 20 astromons

--- app/javascript/components/astromon.js
+++ app/javascript/components/astromon.js
@@ -1,28 +1,34 @@
 const displayGemOnClick = () => {
-  const gem1 = document.getElementById('gem-1');
-  const display1 = document.getElementById('gem-4');
-  const gem2 = document.getElementById('gem-2');
-  const display2 = document.getElementById('gem-5');
-  const gem3 = document.getElementById('gem-3');
-  const display3 = document.getElementById('gem-6');
+  let counter = 1;
+  for (let step = 0; step < 120; step++) {
+    const gem1 = document.getElementById(`gem-${counter}`);
+    const display1 = document.getElementById(`gem-${counter + 3}`);
+    counter++;
+    const gem2 = document.getElementById(`gem-${counter}`);
+    const display2 = document.getElementById(`gem-${counter + 3}`);
+    counter++;
+    const gem3 = document.getElementById(`gem-${counter}`);
+    const display3 = document.getElementById(`gem-${counter + 3}`);
+    counter = counter + 4;
 
-  gem1.addEventListener('click', () => {
-    display1.classList.remove('invisible');
-    display2.classList.add('invisible');
-    display3.classList.add('invisible');
-  });
+    gem1.addEventListener('click', () => {
+      display1.classList.remove('invisible');
+      display2.classList.add('invisible');
+      display3.classList.add('invisible');
+    });
 
-  gem2.addEventListener('click', () => {
-    display1.classList.add('invisible');
-    display2.classList.remove('invisible');
-    display3.classList.add('invisible');
-  });
+    gem2.addEventListener('click', () => {
+      display1.classList.add('invisible');
+      display2.classList.remove('invisible');
+      display3.classList.add('invisible');
+    });
 
-  gem3.addEventListener('click', () => {
-    display1.classList.add('invisible');
-    display2.classList.add('invisible');
-    display3.classList.remove('invisible');
-  });
+    gem3.addEventListener('click', () => {
+      display1.classList.add('invisible');
+      display2.classList.add('invisible');
+      display3.classList.remove('invisible');
+    });
+  }
 }
 
 export { displayGemOnClick };
