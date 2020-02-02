364fd31fb11f51372072c727b25f5641fc4abb3a
Leon Lee <jtllee1@gmail.com>
2020-02-02 01:19:27 +0000

    changed javascript to allow to display gems for all astromons

--- app/javascript/components/astromon.js
+++ app/javascript/components/astromon.js
@@ -1,34 +1,29 @@
 const displayGemOnClick = () => {
-  let counter = 1;
-  for (let step = 0; step < 120; step++) {
-    const gem1 = document.getElementById(`gem-${counter}`);
-    const display1 = document.getElementById(`gem-${counter + 3}`);
-    counter++;
-    const gem2 = document.getElementById(`gem-${counter}`);
-    const display2 = document.getElementById(`gem-${counter + 3}`);
-    counter++;
-    const gem3 = document.getElementById(`gem-${counter}`);
-    const display3 = document.getElementById(`gem-${counter + 3}`);
-    counter = counter + 4;
+  const astromonIds = document.querySelectorAll('.astromon-id');
+  astromonIds.forEach(function(element) {
+    let astromonId = element.innerText;
+    let display1 = document.getElementById(`${astromonId}-4`);
+    let display2 = document.getElementById(`${astromonId}-5`);
+    let display3 = document.getElementById(`${astromonId}-6`);
 
-    gem1.addEventListener('click', () => {
+    document.getElementById(`${astromonId}-1`).addEventListener('click', () => {
       display1.classList.remove('invisible');
       display2.classList.add('invisible');
       display3.classList.add('invisible');
     });
 
-    gem2.addEventListener('click', () => {
+    document.getElementById(`${astromonId}-2`).addEventListener('click', () => {
       display1.classList.add('invisible');
       display2.classList.remove('invisible');
       display3.classList.add('invisible');
     });
 
-    gem3.addEventListener('click', () => {
+    document.getElementById(`${astromonId}-3`).addEventListener('click', () => {
       display1.classList.add('invisible');
       display2.classList.add('invisible');
       display3.classList.remove('invisible');
     });
-  }
+  });
 }
 
 export { displayGemOnClick };
