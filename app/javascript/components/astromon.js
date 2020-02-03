8f440d7ee5b3bb5864ef15cda3ef5ac8602de607
Leon Lee <jtllee1@gmail.com>
2020-02-03 21:11:39 +0000

    added default card with instructions

--- app/javascript/components/astromon.js
+++ app/javascript/components/astromon.js
@@ -5,23 +5,27 @@ const displayGemOnClick = () => {
     let display1 = document.getElementById(`${astromonId}-4`);
     let display2 = document.getElementById(`${astromonId}-5`);
     let display3 = document.getElementById(`${astromonId}-6`);
+    let display4 = document.getElementById(`${astromonId}-7`);
 
     document.getElementById(`${astromonId}-1`).addEventListener('click', () => {
-      display1.classList.remove('invisible');
-      display2.classList.add('invisible');
+      display1.classList.add('invisible');
+      display2.classList.remove('invisible');
       display3.classList.add('invisible');
+      display4.classList.add('invisible');
     });
 
     document.getElementById(`${astromonId}-2`).addEventListener('click', () => {
       display1.classList.add('invisible');
-      display2.classList.remove('invisible');
-      display3.classList.add('invisible');
+      display2.classList.add('invisible');
+      display3.classList.remove('invisible');
+      display4.classList.add('invisible');
     });
 
     document.getElementById(`${astromonId}-3`).addEventListener('click', () => {
       display1.classList.add('invisible');
       display2.classList.add('invisible');
-      display3.classList.remove('invisible');
+      display3.classList.add('invisible');
+      display4.classList.remove('invisible');
     });
   });
 }
