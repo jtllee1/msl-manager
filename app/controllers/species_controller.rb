0677563a0d99d4f766c99190d989c481203079c5
Leon Lee <jtllee1@gmail.com>
2020-02-05 00:18:30 +0000

    fixed bug

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -2,9 +2,9 @@ class SpeciesController < ApplicationController
   def index
     if params[:query].present?
       sql_query = "name ILIKE :query OR element ILIKE :query"
-      @species = Specie.where(sql_query, query: "%#{params[:query]}%")
+      @species = Specie.where(sql_query, query: "%#{params[:query]}%").order(:id).paginate(page: params[:page], per_page: 30)
     else
-      @species = Specie.paginate(page: params[:page], per_page: 30)
+      @species = Specie.order(:id).paginate(page: params[:page], per_page: 30)
     end
   end
 end
