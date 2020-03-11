ff2116dd176f3fe47f00b3b891ace678c57e260b
Leon Lee <jtllee1@gmail.com>
2020-03-11 23:41:36 +0000

    changed gem equip selection to highlight selected gem and removed checkbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,8 @@
 import "bootstrap";
 import { displayGemOnClick } from '../components/astromon';
 import { gemEffectOnLoad } from '../components/gem-effect';
+import { gemSelectOnClick } from '../components/gem-select';
+
 displayGemOnClick();
 gemEffectOnLoad();
+gemSelectOnClick();
