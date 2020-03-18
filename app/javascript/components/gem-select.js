192a48baca0a7c8d82d3474f8e176a6fb4d42a60
Leon Lee <jtllee1@gmail.com>
2020-03-18 01:45:45 +0000

    added check to allow only gems in new page to be highlighted

--- app/javascript/components/gem-select.js
+++ app/javascript/components/gem-select.js
@@ -1,10 +1,13 @@
 const gemSelectOnClick = () => {
-  const gems = document.querySelectorAll('.gem-card');
-  gems.forEach(function(element) {
-    element.addEventListener('click', () => {
-      element.classList.toggle('gem-effect');
+  const check = document.getElementById(`select`).innerText;
+  if (check) {
+    const gems = document.querySelectorAll('.gem-card');
+    gems.forEach(function(element) {
+      element.addEventListener('click', () => {
+        element.classList.toggle('gem-effect');
+      });
     });
-  });
+  };
 }
 
 export { gemSelectOnClick };
