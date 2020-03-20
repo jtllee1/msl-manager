ee04872cbdbd5f9cafdb068a1d61b4a585f8feea
Leon Lee <jtllee1@gmail.com>
2020-03-20 23:31:49 +0000

    added highlight to selected filter

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,11 @@ import "bootstrap";
 import { displayGemOnClick } from '../components/astromon';
 import { gemEffectOnLoad } from '../components/gem-effect';
 import { gemSelectOnClick } from '../components/gem-select';
+import { filterSelectOnClick } from '../components/filter-select';
 import { switchOnClick } from '../components/switch';
 
 displayGemOnClick();
 gemEffectOnLoad();
 switchOnClick();
+filterSelectOnClick();
 gemSelectOnClick();
