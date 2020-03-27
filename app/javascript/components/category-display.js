a9168b0fcc35cb38f6936275caa7656c48cd5a51
Leon Lee <jtllee1@gmail.com>
2020-03-27 21:20:15 +0000

    fixed javascript error from category toggle on other pages

--- app/javascript/components/category-display.js
+++ app/javascript/components/category-display.js
@@ -1,8 +1,10 @@
 const categoryDisplayOnClick = () => {
   const category = document.getElementById(`category-toggle`);
-  category.addEventListener('click', () => {
-    document.getElementById(`category-info`).classList.toggle('invisible');
-  });
+  if (category) {
+    category.addEventListener('click', () => {
+      document.getElementById(`category-info`).classList.toggle('invisible');
+    });
+  };
 }
 
 export { categoryDisplayOnClick };
